ddb019c3807aa81bacaaf02ec4a9e693b506b5d7
jorgemex <jorge.eleven19@gmail.com>
2019-07-23 18:18:34 -0500

    integrando typescript

---
+++ js/main.ts
@@ -0,0 +1,8 @@
+namespace main {
+  export const appName = "Intro";
+  export const Vernumber = "0.1.0";
+
+  export function ini() {
+    console.log("iniciando", appName, Vernumber);
+  }
+}
---
+++ public/js/main.js
@@ -0,0 +1,10 @@
+var main;
+(function (main) {
+    main.appName = "Intro";
+    main.Vernumber = "0.1.0";
+    function ini() {
+        console.log("iniciando", main.appName, main.Vernumber);
+    }
+    main.ini = ini;
+})(main || (main = {}));
+//# sourceMappingURL=main.js.map
